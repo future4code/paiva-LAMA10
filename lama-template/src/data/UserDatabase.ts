c20df61c9c7319adaac8a5f5e6ad2579d6f31c0c
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-08-27 12:57:05 -0300

    Cadastro

--- lama-template/src/data/UserDatabase.ts
+++ lama-template/src/data/UserDatabase.ts
@@ -3,7 +3,7 @@ import { User } from "../model/User";
 
 export class UserDatabase extends BaseDatabase {
 
-  private static TABLE_NAME = "";
+  private static TABLE_NAME = "NOME_TABELAS_USUÁRIOS";
 
   public async createUser(
     id: string,
